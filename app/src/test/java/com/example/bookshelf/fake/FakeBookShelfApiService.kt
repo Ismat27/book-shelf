3019643a35d7ff6396d23ed0efec1640278db579
ismat27 <adegbiteismail27@gmail.com>
2023-12-18 14:24:25 +0100

    Dependency class for testing

---
+++ app/src/test/java/com/example/bookshelf/fake/FakeBookShelfApiService.kt
@@ -0,0 +1,22 @@
+package com.example.bookshelf.fake
+
+import com.example.bookshelf.models.BookDetail
+import com.example.bookshelf.models.BookSearch
+import com.example.bookshelf.network.BookShelfApiService
+
+class FakeBookShelfApiService : BookShelfApiService {
+    override suspend fun searchBooks(query: String): BookSearch {
+        return FakeDataSource.BookSearch
+    }
+
+    override suspend fun getBookDetails(bookId: String): BookDetail {
+        val book = FakeDataSource.books.find { item -> item.id == bookId }
+        return BookDetail(
+            etag = "etag $bookId",
+            id = bookId,
+            kind = "book",
+            volumeInfo = book!!.volumeInfo,
+            selfLink = book.selfLink
+        )
+    }
+}
